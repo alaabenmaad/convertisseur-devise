3395c906ffe2aa07e28b2cf0e38e57bd269e6c88
alaa ben maad <alabenmaad1@gmail.com>
2022-11-04 18:28:31 +0100

    added another chexbox

--- app/src/main/java/com/example/convertisseurdedevise/MainActivity.java
+++ app/src/main/java/com/example/convertisseurdedevise/MainActivity.java
@@ -21,12 +21,11 @@ import java.util.Iterator;
 import java.util.List;
 
 
-
 public class MainActivity extends AppCompatActivity {
 
     public static BreakIterator data;
     List<String> keysList;
-    Spinner toCurrency;
+    Spinner toCurrency, fromCurrency;
     TextView textView;
 
     @Override
@@ -34,37 +33,45 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        toCurrency = (Spinner)findViewById(R.id.planets_spinner);
+        fromCurrency = (Spinner) findViewById(R.id.planets_spinner);
+        ArrayList<String> from = new ArrayList<>();
+        from.add("DT");
+        from.add("EURO");
+        from.add("USD");
+        ArrayAdapter<String> arrayAdapterto = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, from);
+        arrayAdapterto.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        fromCurrency.setAdapter(arrayAdapterto);
+
+        toCurrency = (Spinner) findViewById(R.id.planets_spinner2);
         ArrayList<String> to = new ArrayList<>();
+        to.add("DT");
         to.add("EURO");
         to.add("USD");
-        ArrayAdapter<String> arrayAdapterto = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item, to);
-        arrayAdapterto.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        toCurrency.setAdapter(arrayAdapterto);
+        ArrayAdapter<String> arrayAdapterfrom = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, to);
+        arrayAdapterfrom.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        toCurrency.setAdapter(arrayAdapterfrom);
 
-        final EditText edtDinarValue = (EditText)findViewById(R.id.editText4);
-        final Button btnConvert = (Button)findViewById(R.id.button);
-        textView =(TextView) findViewById(R.id.textView7);
+        final EditText edtDinarValue = (EditText) findViewById(R.id.editText4);
+        final Button btnConvert = (Button) findViewById(R.id.button);
+        textView = (TextView) findViewById(R.id.textView7);
 
 
         btnConvert.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(!edtDinarValue.getText().toString().isEmpty())
-                {
+                if (!edtDinarValue.getText().toString().isEmpty()) {
                     String toCurr = toCurrency.getSelectedItem().toString();
+                    String fromCurr = fromCurrency.getSelectedItem().toString();
                     double dVlaue = Double.valueOf(edtDinarValue.getText().toString());
 
-                    Toast.makeText(MainActivity.this, "Please Wait..", Toast.LENGTH_SHORT).show();
+
                     try {
-                        convertCurrency(toCurr, dVlaue);
+                        convertCurrency(fromCurr, toCurr, dVlaue);
                     } catch (Exception e) {
                         e.printStackTrace();
                         Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
                     }
-                }
-                else
-                {
+                } else {
                     Toast.makeText(MainActivity.this, "Please Enter a Value to Convert..", Toast.LENGTH_SHORT).show();
                 }
 
@@ -73,16 +80,81 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void convertCurrency(final String toCurr, final double euroVlaue)  {
+    public void convertCurrency(final String fromCurr, final String toCurr, final double euroVlaue) {
 
-        if (toCurr == "EURO"){
-            double output = euroVlaue/0.32;
-            textView.setText(String.valueOf(output));
+        switch (fromCurr) {
+            case "DT":
+                switch (toCurr) {
+                    case "DT": {
+                        double sum = euroVlaue;
+                        String output = "votre montant en DT est : " + sum;
+                        textView.setText(output);
+                        break;
+                    }
+                    case "EURO": {
+                        double sum = euroVlaue / 3.2;
+                        String output = "votre montant TND en Euro  est : " + sum;
+                        textView.setText(output);
+                        break;
+                    }
+                    case "USD": {
+                        double sum = euroVlaue / 3;
+                        String output = "votre montant TND en USD est : " + sum;
+                        textView.setText(output);
+
+                        break;
+                    }
+                }
+
+                break;
+            case "EURO":
+                switch (toCurr) {
+                    case "DT": {
+                        double sum = euroVlaue * 3;
+                        String output = "votre montant EURO en TND est : " + sum;
+                        textView.setText(output);
+                        break;
+                    }
+                    case "EURO": {
+                        double sum = euroVlaue;
+                        String output = "votre montant de TND en Euro  est : " + sum;
+                        textView.setText(output);
+                        break;
+                    }
+                    case "USD": {
+                        double sum = euroVlaue * 1;
+                        String output = "votre montant de TND en USD est : " + sum;
+                        textView.setText(output);
+
+                        break;
+                    }
+                }
+
+                break;
+            case "USD":
+                switch (toCurr) {
+                    case "DT": {
+                        double sum = euroVlaue * 3.2;
+                        String output = "votre montant USD en dinars est : " + sum;
+                        textView.setText(output);
+                        break;
+                    }
+                    case "EURO": {
+                        double sum = euroVlaue * 1;
+                        String output = "votre montant USD en Euro  est : " + sum;
+                        textView.setText(output);
+                        break;
+                    }
+                    case "USD": {
+                        double sum = euroVlaue;
+                        String output = "votre montant en USD est : " + sum;
+                        textView.setText(output);
+                        break;
+                    }
+                }
+                break;
         }
-        if (toCurr == "USD"){
-            double output = euroVlaue/0.31;
-            textView.setText(String.valueOf(output));
-        }
+
 
     }
 }
